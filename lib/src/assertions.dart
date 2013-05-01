2f1d27823eef4db0a550729dcf981f14f835fb5e
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-05-01 18:20:17 +0200

    Refacto move code to AbtractClass and use genreric

--- lib/src/assertions.dart
+++ lib/src/assertions.dart
@@ -6,14 +6,49 @@ ObjectAssert assertThisObject(Object o) => new ObjectAssert(o);
  * Base class for all assertions.
  * 
  */
-abstract class AbstractAssert {
+abstract class AbstractAssert<T> {
   
-  Object _actual;
+  T _actual;
   
-  AbstractAssert(this._actual);  
+  AbstractAssert(this._actual);
+  
+  isEqualTo(T other) {
+    if(_actual != other){
+      throw new AssertionFailed("Expecting:\n $_actual\nto be equal to:\n $other\nbut was not.");
+    }
+  }  
+  
+  isNotEqualTo(T other) {
+    if(_actual == other){
+      throw new AssertionFailed("Expecting:\n $_actual\nnot to be equal to:\n$other\n.");
+    }
+  }
+
+  isNull() {
+    if(_actual != null){
+      throw new AssertionFailed("Expecting null but was:\n$_actual.");
+    }
+  }
+
+  isNotNull() {
+    if(_actual == null){
+      throw new AssertionFailed("Expecting to be not null.");
+    }
+  }
+
+  isSameAs(T expected) {
+    if(!identical(_actual, expected)){
+      throw new AssertionFailed("Expecting:\n $_actual\n to be same as:\n$expected\n but was not.");
+    }
+  }
+  
+  isNotSameAs(T expected) {
+    if(identical(_actual, expected)){
+      throw new AssertionFailed("Expecting:\n $_actual\n to be same as:\n$expected");
+    }
+  }  
   
 }
-
 
 /**
  * Throw when assertion fail.
