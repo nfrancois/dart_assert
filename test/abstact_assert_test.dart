2d738891400c9534ad0e6039400d56a98ccce19d
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-05-01 22:20:08 +0200

    typo

--- test/abstact_assert_test.dart
+++ test/abstact_assert_test.dart
@@ -11,18 +11,18 @@ run_object_assert_test() =>
   group('DummyAssert Test',(){
     test('isNotNul ok', () {
       // Given
-      var DummyAssert = new DummyAssert("Dart");
+      var dummyAssert = new DummyAssert("Dart");
       
       // Then
-      DummyAssert.isNotNull();
+      dummyAssert.isNotNull();
     });
     test('isNotNul ko', () {
       // Given
-      var DummyAssert = new DummyAssert(null);
+      var dummyAssert = new DummyAssert(null);
       
       // Then
       try {
-        DummyAssert.isNotNull();
+        dummyAssert.isNotNull();
         failWithoutAssertionFailed();
       } on AssertionFailed catch(e){
         expect(e.message, "Expecting to be not null.");
@@ -30,11 +30,11 @@ run_object_assert_test() =>
     });   
     test('isNul ko', () {
       // Given
-      var DummyAssert = new DummyAssert("Dart");
+      var dummyAssert = new DummyAssert("Dart");
       
       // Then
       try {      
-        DummyAssert.isNull();
+        dummyAssert.isNull();
         failWithoutAssertionFailed();
       } on AssertionFailed catch(e){
         expect(e.message, "Expecting null but was:\nDart.");
@@ -42,22 +42,22 @@ run_object_assert_test() =>
     });  
     test('isEqualTo ok', () {
       // Given
-      var DummyAssert = new DummyAssert("Dart");
+      var dummyAssert = new DummyAssert("Dart");
       // Then
-      DummyAssert.isEqualTo("Dart");
+      dummyAssert.isEqualTo("Dart");
     });    
     test('isEqualTo ok', () {
       // Given
-      var DummyAssert = new DummyAssert("Dart");
+      var dummyAssert = new DummyAssert("Dart");
       
-      DummyAssert.isNotEqualTo("dart");
+      dummyAssert.isNotEqualTo("dart");
     });    
     test('isNotEqualTo ko', () {
       // Given
-      var DummyAssert = new DummyAssert("Dart");
+      var dummyAssert = new DummyAssert("Dart");
       
       try {      
-        DummyAssert.isEqualTo("dart");
+        dummyAssert.isEqualTo("dart");
         failWithoutAssertionFailed();
       } on AssertionFailed catch(e){
         expect(e.message, "Expecting:\n Dart\nto be equal to:\n dart\nbut was not.");
@@ -65,22 +65,22 @@ run_object_assert_test() =>
     }); 
     test('isSameAs ok', () {
       // Given
-      var Dart = "Dart";
-      var DummyAssert = new DummyAssert(Dart);
+      var dart = "Dart";
+      var dummyAssert = new DummyAssert(dart);
       
       // Then
-      DummyAssert.isSameAs(Dart);
+      dummyAssert.isSameAs(dart);
     });    
     test('isSameAs ko', () {
       // Given
-      var Dart = "Dart";
-      var Dart2 = new String.fromCharCodes("Dart".codeUnits);// equals but not are not same
-      var DummyAssert = new DummyAssert(Dart);
+      var dart = "Dart";
+      var dart2 = new String.fromCharCodes("Dart".codeUnits);// equals but not are not same
+      var dummyAssert = new DummyAssert(dart);
       
       // Then
-      expect(Dart, Dart2);
+      expect(dart, dart2);
       try {           
-        DummyAssert.isSameAs(Dart2);
+        dummyAssert.isSameAs(dart2);
         failWithoutAssertionFailed();
       } on AssertionFailed catch(e){
         expect(e.message, "Expecting:\n Dart\n to be same as:\nDart\n but was not.");
@@ -88,26 +88,26 @@ run_object_assert_test() =>
     });   
     test('isNotSameAs ok', () {
       // Given
-      var Dart = "Dart";
-      var Dart2 = new String.fromCharCodes("Dart".codeUnits);// equals but not are not same
-      var DummyAssert = new DummyAssert(Dart);
+      var dart = "Dart";
+      var dart2 = new String.fromCharCodes(dart.codeUnits);// equals but not are not same
+      var dummyAssert = new DummyAssert(dart);
       
       // Then
-      expect(Dart, Dart2);
-      DummyAssert.isNotSameAs(Dart2);
+      expect(dart, dart2);
+      dummyAssert.isNotSameAs(dart2);
     });    
     test('isNotSameAs ko', () {
       // Given
-      var Dart = "Dart";
-      var DummyAssert = new DummyAssert(Dart);
+      var dart = "Dart";
+      var dummyAssert = new DummyAssert(dart);
       
       // Then
       try {           
-        DummyAssert.isNotSameAs(Dart);
+        dummyAssert.isNotSameAs(dart);
         failWithoutAssertionFailed();
       } on AssertionFailed catch(e){
         expect(e.message, "Expecting:\n Dart\n to be same as:\nDart");
       }      
-    });     
+    });  
     
   });
