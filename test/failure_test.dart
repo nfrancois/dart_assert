bc79b2f2174ca6b6ef220916096a1c372c496718
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-05-08 00:31:25 +0200

    Add condition

--- test/failure_test.dart
+++ test/failure_test.dart
@@ -5,12 +5,14 @@ part of dart_assert_test;
 class UnbelievableError {}
 
 run_failure_test(){
-  test('failWhenException', (){
-    try {
-      failBecauseWasNotThrown(UnbelievableError);
-      failWithoutAssertionFailed();
-    } on AssertionFailed catch(e){
-      expect(e.message, "Expected UnbelievableError to be thrown");
-    }
-  });
+  group('Error', () =>
+    test('failWhenException', (){
+      try {
+        failBecauseWasNotThrown(UnbelievableError);
+        failWithoutAssertionFailed();
+      } on AssertionFailed catch(e){
+        expect(e.message, "Expected UnbelievableError to be thrown");
+      }
+    })
+   );
 }
