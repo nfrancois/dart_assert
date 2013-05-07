370681330fd7ebb03d10d7c3558a635dc0625668
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-05-08 00:32:16 +0200

    rename file

--- test/dart_assert_test.dart
+++ test/dart_assert_test.dart
@@ -5,7 +5,7 @@ library dart_assert_test;
 import 'package:unittest/unittest.dart';
 import 'package:dart_assert/dart_assert.dart';
 
-part 'abstact_assert_test.dart';
+part 'abstract_assert_test.dart';
 part 'failure_test.dart';
 
 main(){
